6aa3210f1277611effa5ce6e0ac848b660679558
brunosana <brunosa@dcomp.ufs.br>
2022-04-12 15:21:43 -0300

    fix: PORT env

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
     logInjection: true,
   });
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
